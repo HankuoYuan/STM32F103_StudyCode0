e4f85656e73896a392d1911a3b757601d06b8e77
HankuoYuan <yuanhankuo@qq.com>
2023-11-18 01:16:30 +0800

    Initial commit

---
+++ Core/Src/main.c
@@ -0,0 +1,330 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "i2c.h"
+#include "tim.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+#ifdef TASK3
+uint8_t a;
+#endif
+#ifdef TASK5
+uint16_t compare = 0;
+#endif
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+#ifdef TASK3
+
+uint16_t FIBONACCI(uint8_t a);
+
+#endif
+#ifdef TASK4
+
+//void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim);
+
+#endif
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void) {
+    /* USER CODE BEGIN 1 */
+
+    /* USER CODE END 1 */
+
+    /* MCU Configuration--------------------------------------------------------*/
+
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
+
+    /* USER CODE BEGIN Init */
+
+    /* USER CODE END Init */
+
+    /* Configure the system clock */
+    SystemClock_Config();
+
+    /* USER CODE BEGIN SysInit */
+
+    /* USER CODE END SysInit */
+
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_I2C1_Init();
+    MX_TIM2_Init();
+    /* USER CODE BEGIN 2 */
+#ifndef TASK5
+#ifndef TASK4
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);
+    GPIO_InitStruct.Pin = GPIO_PIN_0;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+#endif
+#endif
+#ifdef TASK4
+    //    HAL_TIM_Base_Start_IT(&htim2);
+        HAL_TIM_Base_Start(&htim2);
+        HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
+#endif
+#ifdef TASK5
+    //    HAL_TIM_Base_Start_IT(&htim2);
+    HAL_TIM_Base_Start(&htim2);
+    HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
+#endif
+    /* USER CODE END 2 */
+
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
+    while (1) {
+        /* USER CODE END WHILE */
+
+        /* USER CODE BEGIN 3 */
+#ifdef TASK1
+        FLOWLED();
+#endif
+#ifdef TASK2
+        if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12) == GPIO_PIN_RESET) {
+            HAL_Delay(80);
+            if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12) == GPIO_PIN_RESET) {           //判断按键按下
+                while (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12) == GPIO_PIN_RESET);
+                HAL_Delay(80);
+                if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12) == GPIO_PIN_SET) {         //判断按键松开，松�?后反转led电平
+                    HAL_GPIO_TogglePin(GPIOA, 0X00FF);
+                }
+            }
+        }
+        if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13) == GPIO_PIN_RESET) {
+            HAL_Delay(80);
+            if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13) == GPIO_PIN_RESET) {           //判断按键按下
+                while (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13) == GPIO_PIN_RESET);
+                HAL_Delay(80);
+                if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13) == GPIO_PIN_SET) {         //判断按键松开，松�?后反转led电平
+                    while (1) {
+                        FLOWLED();
+                    }
+                }
+            }
+        }
+#endif
+#ifdef TASK3
+        if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_14) == GPIO_PIN_RESET) {
+            HAL_Delay(80);
+            if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_14) == GPIO_PIN_RESET) {           //判断按键按下
+                while (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_14) == GPIO_PIN_RESET);
+                HAL_Delay(80);
+                if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_14) == GPIO_PIN_SET) {         //判断按键松开，松�?后反转led电平
+                    for (a = 1; FIBONACCI(a) <= 0x00FF; a++) {                      //仅在项数值在1-255时亮起化为二进制时相应的LED
+                        HAL_GPIO_TogglePin(GPIOA, FIBONACCI(a));
+                        HAL_Delay(1000);
+                        HAL_GPIO_TogglePin(GPIOA, FIBONACCI(a));
+                    }
+                }
+            }
+        }
+#endif
+#ifdef TASK4
+        for (uint16_t i = 0; i < 1000; ++i) {
+            __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, i);
+            HAL_Delay(1);
+        }
+        HAL_Delay(200);
+        for (uint16_t i = 1000; i > 0; --i) {
+            __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, i);
+            HAL_Delay(1);
+        }
+        HAL_Delay(200);
+#endif
+#ifdef TASK5
+        if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12) == GPIO_PIN_RESET) {
+            HAL_Delay(80);
+            if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12) == GPIO_PIN_RESET) {           //判断按键按下
+                while (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12) == GPIO_PIN_RESET) {
+                    while (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12) == GPIO_PIN_RESET) {
+                        if (compare < 1000) {
+                            __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, compare);
+                            compare++;
+                            HAL_Delay(3);
+                        }
+                    }
+                    HAL_Delay(80);
+                }
+            }
+        }
+        if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13) == GPIO_PIN_RESET) {
+            HAL_Delay(80);
+            if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13) == GPIO_PIN_RESET) {           //判断按键按下
+                while (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13) == GPIO_PIN_RESET) {
+                    while (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13) == GPIO_PIN_RESET) {
+                        if (compare > 0) {
+                            __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, compare);
+                            compare--;
+                            HAL_Delay(3);
+                        }
+                    }
+                    HAL_Delay(80);
+                }
+            }
+        }
+#endif
+    }
+    /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void) {
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+    /** Initializes the RCC Oscillators according to the specified parameters
+    * in the RCC_OscInitTypeDef structure.
+    */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
+
+    /** Initializes the CPU, AHB and APB buses clocks
+    */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+                                  | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+        Error_Handler();
+    }
+
+    /** Enables the Clock Security System
+    */
+    HAL_RCC_EnableCSS();
+}
+
+/* USER CODE BEGIN 4 */
+#ifdef TASK3
+
+uint16_t FIBONACCI(uint8_t a) {
+    uint16_t b = 1, c = 1, d = 0;
+    /**注意变量类型的大小，只能大不能小，否则会输出随机范围数！
+    */
+    if (a == 1) {
+        return 1;
+    } else if (a == 2) {
+        return 1;
+    } else if (a > 2) {
+        for (int j = 2; j < a; j++) {
+            d = b + c;
+            b = c;
+            c = d;
+        }
+        return c;
+    } else {
+        return 0;
+    }
+}
+
+#endif
+#ifdef TASK4
+
+//void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+//    if (htim == &htim2) {
+//        //500ms trigger
+//        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+//    }
+//}
+
+#endif
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void) {
+    /* USER CODE BEGIN Error_Handler_Debug */
+    /* User can add his own implementation to report the HAL error return state */
+    __disable_irq();
+    while (1) {
+    }
+    /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
